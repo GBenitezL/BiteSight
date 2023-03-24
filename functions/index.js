eda3d8f0f846dc3c0ad65d4f0bb6479112216c71
Gustavo Benitez <benitezlgustavo@gmail.com>
2023-03-24 03:01:03 -0600

    Route Setup

---
+++ functions/index.js
@@ -0,0 +1,30 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const serviceAccount = require("./permissions.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+app.use(cors({ origin: true }));
+
+
+// Routes
+app.use(require("./routes/userRoutes"));
+app.use(require("./routes/restaurantRoutes"));
+
+exports.app = functions.https.onRequest(app);
+
+// // Create and deploy your first functions
+// // https://firebase.google.com/docs/functions/get-started
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
