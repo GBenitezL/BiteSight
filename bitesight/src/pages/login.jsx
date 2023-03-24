5be70e42d70c1dd1618645496ec2bca8f17d47fa
Juan Carlos Gómez A <49078525+Juancgmz@users.noreply.github.com>
2023-03-24 15:43:09 -0600

    React-Router-Dom Funcionando

---
+++ bitesight/src/pages/login.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Login = () => {
+    return (
+        <div>
+            <p>Hola este es el login</p>
+            <p>Hola este es el login</p>
+            <p>Hola este es el login</p>
+            <p>Hola este es el login</p>
+            <p>Hola este es el login</p>
+            <p>Hola este es el login</p>
+            <p>Hola este es el login</p>
+            <p>Hola este es el login</p>
+            <p>Hola este es el login</p>
+            <p>Hola este es el login</p>
+            <p>Hola este es el login</p>
+        </div>
+    );
+};
+
+export default Login;
