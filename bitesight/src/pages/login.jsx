92b4b8a0b080b6ff561acb0a002fd5b64b8d08d9
Juan Carlos Gómez A <49078525+Juancgmz@users.noreply.github.com>
2023-03-29 12:02:27 -0600

    Initial Columns Login

--- bitesight/src/pages/login.jsx
+++ bitesight/src/pages/login.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import {
-  BrowserView,
-  MobileView,
-  isBrowser,
-  isMobile,
-} from "react-device-detect";
+import "./login.css";
 
 const Login = () => {
   let width = window.innerWidth;
   if (width > 720) {
     return (
-      <div className="container">
-        <h1>Big size</h1>
+      <div className="row">
+        <div className="column" style={{backgroundColor: "#696969"}}>
+          <h2>Column 1</h2>
+          <p>Some text..</p>
+        </div>
+        <div className="column">
+          <h2>Column 2</h2>
+          <p>Some text..</p>
+        </div>
       </div>
     );
   } else {
