ed32bfd5957026d5a6ec4b4ca2fd5b01c75ab73d
Juan Carlos Gómez A <49078525+Juancgmz@users.noreply.github.com>
2023-06-01 22:07:56 -0600

    Grammar Check

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,9 +7,9 @@ export default function Home() {
       <main>
         <h1>Welcome to BiteSight</h1>
         <h2>Who Are We?</h2>
-        <p>We are a group of students from the <strong>Tec de Monterrey</strong>. This web application will be our last big proyect, how exciting!.</p>
+        <p>We are a group of students from Tec de Monterrey. This web application will be our last big project. How exciting!</p>
         <h2>What Is This Project About?</h2>
-        <p>BiteSight is a web application that can help people in Monterrey discover new places to eat or visit. With such a big city, it is almost certain that there are good places we do not know. We want to create a recommendation application that uses Machine Learning in order to target the places that are most likely to match a person based on their likings.</p>
+        <p>BiteSight is a web application that can help people in Monterrey discover new places to eat or visit. With such a big city, it is almost certain that there are good places we do not know about. We want to create a recommendation application that uses Machine Learning to target the places that are most likely to match a person's preferences.</p>
       </main>
     </div>
   );
